3abce42567757bf09fdc0e1b692ee9398a6bb955
Dylan L <dylancblarocque@gmail.com>
2023-04-06 23:23:49 -0700

    Codegen types in reg mutation + err schema

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,46 +1,46 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
-import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-} from '@chakra-ui/form-control';
-import { Input } from '@chakra-ui/input';
+import * as Yup from 'yup';
 import { Button } from '@chakra-ui/button';
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
 import { Box, border } from '@chakra-ui/react';
 import { useMutation } from 'urql';
+import { RegisterDocument } from '../codegen/graphql';
 
 interface registerProps {}
 
-const REGISTER_MUTATION = `
-mutation Register($username: String!, $password: String! ) {
-  register(options: {username: $username, password: $password}) {
-    errors {
-      field
-      message
-    }
-    user {
-      _id
-      username
-    }
-  }
-}
-`;
+const SignupSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+  password: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+});
 
 const Register: React.FC<registerProps> = ({}: registerProps) => {
-  const [, registerFunc] = useMutation(REGISTER_MUTATION);
+  const [, registerFunc] = useMutation(RegisterDocument);
+
   return (
     <Wrapper variant='small'>
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => {
+        validationSchema={SignupSchema}
+        onSubmit={async (values) => {
           console.log(values);
-          return registerFunc(values);
+          const response = await registerFunc(values);
+          // if (response.data?.register.errors) {
+          //   setErrors()
+          // }
+          // const errors = response.data?.register.errors;
+          // console.log(errors);
+          // const user = response.data?.register.user;
         }}
       >
-        {({ values, handleChange }) => (
+        {({ values, handleChange, errors, touched }) => (
           <Box border='1px' p={8} rounded='md'>
             <Form>
               <InputField
@@ -48,6 +48,9 @@ const Register: React.FC<registerProps> = ({}: registerProps) => {
                 label='Username'
                 placeholder='username'
               />
+              {/* {errors.username && touched.username ? (
+                <div>hey!{errors.username}</div>
+              ) : null} */}
               <Box mt={4}>
                 <InputField
                   name='password'
@@ -57,12 +60,7 @@ const Register: React.FC<registerProps> = ({}: registerProps) => {
                 />
               </Box>
 
-              <Button
-                mt={4}
-                colorScheme='orange'
-                // isLoading={values.isSubmitting}
-                type='submit'
-              >
+              <Button mt={4} colorScheme='orange' type='submit'>
                 Register
               </Button>
             </Form>
