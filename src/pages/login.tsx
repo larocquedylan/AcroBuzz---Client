67f5be14ea2987095ffbbd26c5e07ea182756992
Dylan L <dylancblarocque@gmail.com>
2023-04-07 07:51:44 -0700

    Login UI complete

---
+++ src/pages/login.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+import { Button } from '@chakra-ui/button';
+import Wrapper from '../components/Wrapper';
+import InputField from '../components/InputField';
+import { Box, border } from '@chakra-ui/react';
+import { useMutation } from 'urql';
+import { LoginDocument } from '../codegen/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
+
+const Login: React.FC<{}> = ({}) => {
+  const [, loginFunc] = useMutation(LoginDocument);
+  const router = useRouter();
+
+  return (
+    <Wrapper variant='small'>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await loginFunc(values);
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            // worked
+            router.push('/');
+          }
+        }}
+      >
+        {({ values }) => (
+          <Box border='1px' p={8} rounded='md'>
+            <Form>
+              <InputField
+                name='username'
+                label='Username'
+                placeholder='username'
+              />
+              <Box mt={4}>
+                <InputField
+                  name='password'
+                  label='Password'
+                  placeholder='password'
+                  type='password'
+                />
+              </Box>
+
+              <Button mt={4} colorScheme='orange' type='submit'>
+                Login
+              </Button>
+            </Form>
+          </Box>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
