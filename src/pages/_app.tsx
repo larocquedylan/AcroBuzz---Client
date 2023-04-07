ad047923c4d7e982ff7b81ea3cd1629ac9ec198f
Dylan L <dylancblarocque@gmail.com>
2023-04-06 18:21:54 -0700

    Add urql client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,34 @@
-import { ChakraProvider } from '@chakra-ui/react'
-
-import theme from '../theme'
-import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from '../theme';
+import { AppProps } from 'next/app';
+import { Client, cacheExchange, fetchExchange, Provider } from 'urql';
+// import { ApolloProvider } from '@apollo/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // const apolloClient = useApollo(pageProps)
+  const client = new Client({
+    url: 'http://localhost:8080/graphql',
+    exchanges: [cacheExchange, fetchExchange],
+    // include cookies -> credentials: 'include'
+    fetchOptions() {
+      return {
+        credentials: 'include',
+      };
+    },
+  });
+
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Provider value={client}>
+        <Component {...pageProps} />
+      </Provider>
+      {/* <ApolloProvider client={client}> */}
+      {/* <ClientOnly> */}
+      {/* <Component {...pageProps} /> */}
+      {/* </ClientOnly> */}
+      {/* </ApolloProvider> */}
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
