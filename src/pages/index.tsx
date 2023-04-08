c40793ac793bd70a79ea341f493445927e8190c4
Dylan L <dylancblarocque@gmail.com>
2023-04-08 10:27:21 -0700

    Posts query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,25 @@
 import NavBar from '../components/NavBar';
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+import { PostsDocument } from '../codegen/graphql';
+import { useQuery } from 'urql';
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div> Hello World</div>
-  </>
-);
+const Index = () => {
+  const [{ data, fetching }] = useQuery({
+    query: PostsDocument,
+  });
 
-export default Index;
+  return (
+    <>
+      <NavBar />
+      <div> Hello World</div>
+      {!data && fetching ? (
+        <p>loading.. </p>
+      ) : (
+        data.posts.map((post) => <div key={post._id}> {post.title}</div>)
+      )}
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
