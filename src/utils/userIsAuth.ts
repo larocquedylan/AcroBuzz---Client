d744d7f30d07c61c30f31faeb51bda40721a70c5
Dylan L <dylancblarocque@gmail.com>
2023-04-14 11:18:20 -0700

    make router path into string for routing login

--- src/utils/userIsAuth.ts
+++ src/utils/userIsAuth.ts
@@ -11,7 +11,8 @@ export const userIsAuth = () => {
   useEffect(() => {
     if (!fetching && !data?.me) {
       // If user is not auth, redirect to login page and then back to the page they were on after log in
-      router.replace(`/login=next?/${router.pathname}`);
+      // router.replace('/login?=next' + router.pathname);
+      router.replace(`/login?next=${router.pathname}`);
     }
   }, [data, fetching, router]);
 };
