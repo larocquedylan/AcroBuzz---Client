5dbabd7686a954f1bf4c50b5b9989c1fbbbe5849
Dylan L <dylancblarocque@gmail.com>
2023-04-14 15:39:16 -0700

    getPaginatedPost query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,27 @@
 import NavBar from '../components/NavBar';
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
-import { PostsDocument } from '../codegen/graphql';
+import { GetPaginatedPostsDocument, Exact } from '../codegen/graphql';
 import { useQuery } from 'urql';
 import Layout from '../components/Layout';
 import NextLink from 'next/link';
 import { Link } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const Index = () => {
+  const [variables, setVariables] = useState<
+    Exact<{ cursor?: string; limit?: number }>
+  >({ limit: 10 });
   const [{ data, fetching }] = useQuery({
-    query: PostsDocument,
+    query: GetPaginatedPostsDocument,
+    variables,
   });
+
+  const loadMorePosts = () => {
+    if (data && data.posts.nextCursor) {
+      setVariables({ cursor: data.posts.nextCursor, limit: 10 });
+    }
+  };
 
   return (
     <Layout>
@@ -22,10 +33,44 @@ const Index = () => {
       {!data && fetching ? (
         <p>loading.. </p>
       ) : (
-        data.posts.map((post) => <div key={post.id}> {post.title}</div>)
+        data.posts.posts.map((post) => <div key={post.id}> {post.title}</div>)
+      )}
+      {data && data.posts.nextCursor && (
+        <button onClick={loadMorePosts}>Load More</button>
       )}
     </Layout>
   );
 };
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
+
+// const Index = () => {
+//   const [variables, setVariables] = useState({ input: { limit: 10 } });
+//   const [{ data, fetching }] = useQuery({
+//     query: GetPaginatedPostsDocument,
+//     variables,
+//   });
+
+//   const loadMorePosts = () => {
+//     if (data && data.posts.nextCursor) {
+//       setVariables({ input: { cursor: data.posts.nextCursor, limit: 10 } });
+//     }
+//   };
+
+//   return (
+//     <Layout>
+//       <NextLink href={'/create-post'}>
+//         <Link>create post</Link>
+//       </NextLink>
+
+//       <br />
+//       {!data && fetching ? (
+//         <p>loading.. </p>
+//       ) : (
+//         data.posts.map((post) => <div key={post.id}> {post.title}</div>)
+//       )}
+//     </Layout>
+//   );
+// };
+
+// export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
