084e79e8effcae2e99cc553ceb72dbfd55c98b73
Dylan L <dylancblarocque@gmail.com>
2023-04-16 22:20:27 -0700

    Single Post Page basic

---
+++ src/pages/post/[id].tsx
@@ -0,0 +1,46 @@
+import { withUrqlClient } from 'next-urql';
+import React from 'react';
+import { createUrqlClient } from '../../utils/createUrqlClient';
+import { useRouter } from 'next/router';
+import { useQuery } from 'urql';
+import { PostDocument } from '../../codegen/graphql';
+import Layout from '../../components/Layout';
+import { Box, Heading } from '@chakra-ui/react';
+
+const Post = ({}) => {
+  const router = useRouter();
+  const [{ data, error, fetching }] = useQuery({
+    query: PostDocument,
+    variables: {
+      id: parseInt(router.query.id as string),
+    },
+  });
+
+  if (error) {
+    return (
+      <Layout>
+        <div>{error.message}</div>
+      </Layout>
+    );
+  }
+
+  if (fetching) {
+    return (
+      <Layout>
+        <div>loading...</div>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <Heading>{data?.post?.title}</Heading>
+      <Box>{data?.post?.text}</Box>
+      <div> {data?.post?.author.username}</div>
+      <div> {data?.post?.totalPoints}</div>
+      <div> {data?.post?.createdAt}</div>
+    </Layout>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
