b58e64091467809213a6dedf6ce5afba58771fcf
Dylan L <dylancblarocque@gmail.com>
2023-04-19 13:26:39 -0700

    change url to env

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -74,7 +74,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
   }
 
   return {
-    url: 'http://localhost:8080/graphql',
+    url: process.env.NEXT_PUBLIC_API_URL,
     fetchOptions() {
       if (isServerSide && ctx?.req?.headers?.cookie) {
         return {
