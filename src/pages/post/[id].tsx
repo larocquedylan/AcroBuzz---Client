820f20df34d46bf5fabd207f16b82a19de4bc731
Dylan L <dylancblarocque@gmail.com>
2023-04-24 10:35:17 -0700

    migrate post to apollo

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,19 +1,27 @@
 import { useRouter } from 'next/router';
 import React from 'react';
 import Layout from '../../components/Layout';
-import { Box, Flex, Heading, IconButton } from '@chakra-ui/react';
-import { DeleteIcon } from '@chakra-ui/icons';
-import { useDeletePostMutation, usePostQuery } from '../../codegen/graphql';
+import { Box, Card, Flex, Heading, IconButton } from '@chakra-ui/react';
+import { DeleteIcon, EditIcon } from '@chakra-ui/icons';
+import {
+  useDeletePostMutation,
+  useMeQuery,
+  usePostQuery,
+} from '../../codegen/graphql';
+import NextLink from 'next/link';
 
 const Post = ({}) => {
   const router = useRouter();
+
+  const { data: meData } = useMeQuery();
+
   const { data, error, loading } = usePostQuery({
     variables: {
       id: parseInt(router.query.id as string),
     },
   });
 
-  const [deleteFunc] = useDeletePostMutation();
+  const [deletePost] = useDeletePostMutation();
 
   if (error) {
     return (
@@ -33,19 +41,32 @@ const Post = ({}) => {
 
   return (
     <Layout>
-      <Heading>{data?.post?.title}</Heading>
-      <div> {data?.post?.author.username}</div>
-      <Box>{data?.post?.text}</Box>
-      <div> {data?.post?.createdAt}</div>
-      <Flex>
-        <IconButton
-          aria-label='delete post'
-          icon={<DeleteIcon />}
-          onClick={() => {
-            deleteFunc({ variables: { deletePostId: data?.post?.id } });
-          }}
-        />
-      </Flex>
+      <Card variant={'elevated'} padding={8}>
+        <Heading>{data?.post?.title}</Heading>
+        <div> {data?.post?.author.username}</div>
+        <Box mt={2}>{data?.post?.text}</Box>
+        <div> {data?.post?.createdAt}</div>
+        {meData?.me?.id !== data?.post?.author.id ? null : (
+          <Box mt={2}>
+            <NextLink
+              href='/post/edit/[id]'
+              as={`/post/edit/${data?.post?.id}`}
+            >
+              <IconButton aria-label='edit post' icon={<EditIcon />} />
+            </NextLink>
+            <IconButton
+              aria-label='delete post'
+              ml={4}
+              icon={<DeleteIcon />}
+              onClick={() => {
+                deletePost({
+                  variables: { deletePostId: data?.post?.id },
+                });
+              }}
+            />
+          </Box>
+        )}
+      </Card>
     </Layout>
   );
 };
