edd598dc3593bf892026920e8d1f797aa3330575
Dylan L <dylancblarocque@gmail.com>
2023-04-24 10:42:23 -0700

    sqitch uri to next public api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { PaginatedPosts } from '../codegen/graphql';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: process.env.NEXT_PUBLIC_API_URL,
   credentials: 'include',
   cache: new InMemoryCache({
     typePolicies: {
